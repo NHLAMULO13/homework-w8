676f0dcf38152118d9c925f1c2f4463796a95574
NHLAMULO13 <nhlamulonyalungu@gmail.com>
2025-08-29 15:56:57 +0200

    updated all files

--- app.js
+++ app.js
@@ -2,8 +2,39 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#degrees");
   let temperature = response.data.temperature.current;
   let city = document.querySelector("#city-weather");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}"class="weather-icon"/>`;
+
   city.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  timeElement.innerHTML = formatDate(date);
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function citySearch(city) {
@@ -18,8 +49,15 @@ function handleSearchSubmit(event) {
 
   citySearch(searchInput.value);
 }
+function getForcast(city) {
+  let apiKey = "aa8t7e70fb21d2d41f6fo0b32468a4f3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data);
+
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
   let forecastHtml = "";
   days.forEach(function (day) {
@@ -46,4 +84,4 @@ function displayForecast() {
   searchForm.addEventListener("submit", handleSearchSubmit);
   citySearch("Pretoria");
 }
-displayForecast();
+getForcast("Pretoria");
